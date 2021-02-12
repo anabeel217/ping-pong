dfa2b3218d090a4e28387c62b3b6c708c21f5286
anabeel217 <78968693+anabeel217@users.noreply.github.com>
2021-02-12 20:14:58 +0530

    Add files via upload

---
+++ pong_day_22/main.py
@@ -0,0 +1,60 @@
+# Create the screen
+# Create the paddles - Player and Computer
+# Move the paddles
+# Create the ball
+# Move the ball
+# Detect collision with paddles and wall
+# Detect when paddle misses
+# Create the scoreboard
+# Create the center line
+
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with right paddle and left paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when r_paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+    # Detect when l_paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
+
+
+screen.exitonclick()
